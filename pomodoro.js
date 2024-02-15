3469fb7451c18c6820c8df2f7d75b6eaeb4e2aa6
Begum <94627984+begumaksoy@users.noreply.github.com>
2024-02-15 23:36:49 +0300

    genel düzenleme

--- pomodoro.js
+++ pomodoro.js
@@ -1,5 +1,5 @@
 let time = 1500;
-let i = time;
+let prev_time = time;
 let interval;
 let counter;
 let start_stop;
@@ -8,11 +8,25 @@ let tasksArray = localStorage.getItem("tasks")
     : [];
 let finished_button;
 let pause_button;
+let start_button;
+let add_new_button;
+let pomodoro_button;
+let short_break_button;
+let long_break_button;
+let task_input;
+
+const sound = new Audio("notification.wav");
 window.onload = () => {
+    task_input = document.getElementById("item");
     counter = document.getElementById("counter");
     start_stop = document.getElementById("start-stop");
+    pomodoro_button = document.getElementById("pomodoro_button");
+    short_break_button = document.getElementById("short_break_button");
+    long_break_button = document.getElementById("long_break_button");
     finished_button = document.getElementById("finished_button");
     pause_button = document.getElementById("pause-button");
+    add_new_button = document.getElementById("add_new_button");
+
     tasksArray.forEach(addTask);
 };
 function addTask(text) {
@@ -29,20 +43,50 @@ function add() {
         addTask(input.value);
         input.value = "";
     }
+    finish();
+}
+
+function finish() {
+    clearInterval(interval);
+    pause_button.style = "display: none;";
+    finished_button.style = "display: none;";
+    start_stop.style = "display: block;";
+    time = prev_time;
 }
 function pomodoro_time() {
+    finish();
+    task_input.style = "";
+    short_break_button.style = "";
+    pomodoro_button.style =
+        "background-color: white; border-color: #ff5a5a; border-style: solid; color: #ff5a5a;";
+    long_break_button.style = "";
     counter.innerText = "25:00";
     time = 1500;
+    prev_time = time;
 }
 
 function short_b_time() {
+    finish();
+    task_input.style = "display: none";
+    pomodoro_button.style = "";
+    short_break_button.style =
+        "background-color: white; border-color: #ff5a5a; border-style: solid; color: #ff5a5a;";
+    long_break_button.style = "";
     counter.innerText = "05:00";
     time = 300;
+    prev_time = time;
 }
 
 function long_b_time() {
+    finish();
+    task_input.style = "display: none";
+    short_break_button.style = "";
+    pomodoro_button.style = "";
+    long_break_button.style =
+        "background-color: white; border-color: #ff5a5a; border-style: solid; color: #ff5a5a;";
     counter.innerText = "10:00";
     time = 600;
+    prev_time = time;
 }
 
 function every_time() {
@@ -51,18 +95,30 @@ function every_time() {
         String(Math.floor(time / 60)).padStart(2, "0") +
         ":" +
         String(time % 60).padStart(2, "0");
-    if (time == 0) {
-        clearInterval(interval);
+    if (time <= 0) {
+        sound.play();
+        finish();
     }
 }
 
 function start_time() {
-    interval = setInterval(every_time, 1000);
+    const input = document.getElementById("item");
+    if (time != 1500) {
+        interval = setInterval(every_time, 1000);
 
-    start_stop.style = "display: none;";
-    finished_button.style = "display: inline-block;";
-    pause_button.style = "display: inline-block;";
-    pause_button.innerText = "pause";
+        start_stop.style = "display: none;";
+        pause_button.style = "display: inline-block;";
+        pause_button.innerText = "pause";
+    } else if (time == 1500 && input.value == "") {
+        alert("You should enter a task to start ^v^");
+    } else if (time == 1500 && input.value != "") {
+        interval = setInterval(every_time, 1000);
+
+        start_stop.style = "display: none;";
+        finished_button.style = "display: inline-block;";
+        pause_button.style = "display: inline-block;";
+        pause_button.innerText = "pause";
+    }
 }
 
 function pause() {
